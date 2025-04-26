4f8fea6fd223a6e9bdeab1ed56034b85a7f8225e
Dhanesh R <122768538+DhaneshRamesh@users.noreply.github.com>
2025-04-26 18:41:24 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Or restrict to your Azure domain
+    allow_origins=["https://kind-island-057bb3903.6.azurestaticapps.net"],  # frontend hosted on Azure
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from Backend!"}
